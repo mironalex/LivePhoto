7ce91e947262feac7ae5c194e123c2e66298edd5
Alexandru Miron <mironalex96@gmail.com>
2018-05-23 17:15:58 +0300

    added an activity for gif viewing

---
+++ app/src/main/java/com/thedesert/fox/livephoto/Gallery/GifActivity.java
@@ -0,0 +1,14 @@
+package com.thedesert.fox.livephoto.Gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.thedesert.fox.livephoto.R;
+
+public class GifActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gif);
+    }
+}
